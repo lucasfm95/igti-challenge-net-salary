b1f55cf51a7faa17162a3dda66d094d7fdfe21d0
Lucas Martins <martinslf@taginfraestrutura.com.br>
2020-08-12 21:15:42 -0300

    added formater number

--- src/components/NetSalary/NetSalary.js
+++ src/components/NetSalary/NetSalary.js
@@ -4,6 +4,7 @@ import ProcessedData from "../ProcessedData/ProcessedData";
 import StatusBar from "../StatusBar/StatusBar";
 import css from './net-salary.module.css';
 import { calculateSalaryFrom } from '../../helpers/salary';
+import { formatNumber } from '../../helpers/format';
 
 export default class NetSalary extends Component {
 
@@ -27,7 +28,7 @@ export default class NetSalary extends Component {
 
         this.setState({
             baseINSS: result.baseINSS,
-            discountINSS: result.baseINSS,
+            discountINSS: result.discountINSS,
             baseIR: result.baseIRPF,
             discountIR: result.discountIRPF,
             netSalary: result.netSalary
@@ -40,7 +41,12 @@ export default class NetSalary extends Component {
             <div>
                 <h1 className={css.textAlign}>Net Salary</h1>
                 <InputSalary label="Salary" step="100" value="1000" onChange={this.handleOnChangeInput} />
-                <ProcessedData baseINSS={baseINSS} discountINSS={discountINSS} baseIR={baseIR} discountIR={discountIR} netSalary={netSalary} />
+                <ProcessedData
+                    baseINSS={formatNumber(baseINSS)}
+                    discountINSS={formatNumber(discountINSS)}
+                    baseIR={formatNumber(baseIR)}
+                    discountIR={formatNumber(discountIR)}
+                    netSalary={formatNumber(netSalary)} />
                 <StatusBar />
             </div>
         )
